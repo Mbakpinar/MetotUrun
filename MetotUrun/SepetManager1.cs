d497292eb9dcc67662a74e1f3fa7c4620bee812c
Barkın Akpınar <barkin.ba52@gmail.com>
2021-01-24 16:15:56 +0300

    20 ocak

---
+++ MetotUrun/SepetManager1.cs
@@ -0,0 +1,22 @@
+﻿using MetotUrun;
+using System;
+
+namespace Methods
+{
+    internal class SepetManager
+    {
+        public SepetManager()
+        {
+        }
+
+        internal void Ekle(Urun urun2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Ekle2(string v1, string v2, int v3, int v4)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MetotUrun/sepetManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetotUrun
+
+{
+    class sepetManager
+    {
+        //naming convention
+        //syntax
+        public void Ekle(Urun urun) //İşlem tanımlandı (işlemin uygulanacağı parametre)
+        {
+            Console.WriteLine("Tebrikler! Sepete eklendi : " + urun.Adi); //Tanımlanan işlem yürütüldü.
+        }
+
+
+        //bu şekilde kullanma!! aynı şekilde yazmazsak hata alırız.
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int StokAdedi)
+        {
+            Console.WriteLine("Tebrikler! Sepete eklendi : " + urunAdi);
+        }
+    }
+}
