8580ce4eff5aab452e0da72d7acc3e8ff2b2e5d3
Barkın Akpınar <barkin.ba52@gmail.com>
2021-01-24 16:16:44 +0300

    20 Ocak

---
+++ Oop2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Barkın
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Barkın";
+            musteri1.Soyadi = "Akpınar";
+            musteri1.TcNo = "1245678910";
+
+            // kodlama.io
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "KODLAMA.IO";
+            musteri2.VergiNo = "12345678";
+
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+
+
+
+
+
+
+
+            //GERÇEK--TÜZEL
+        }
+    }
+}
